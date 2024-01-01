933feb84c22153ea862f404a16ef755a951c75b6
wanghehan <524259885@qq.com>
2024-01-01 14:01:56 +0800

    feat: assitants api

---
+++ assistant/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+print("model api init OPENAI_BASE_URL, OPENAI_API_KEY in __init__.py")
+
+os.environ['OPENAI_BASE_URL'] = 'https://key.wenwen-ai.com/v1'
+os.environ['OPENAI_API_KEY'] = 'sk-6V2exWFBSa2lmuZ7C0D773D1BaEd4fB7A1B6A0A265D550C6'
